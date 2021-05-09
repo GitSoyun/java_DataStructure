2e3b386d87e8cabc9e22c7602279e4be71a82c4b
GitSoyun <pinktenshi@naver.com>
2021-05-09 22:05:01 +0900

    SinglyLinkedListQueue를 위한 SinglyNode 구현 [by 정소윤]

---
+++ data_04_Queue/SinglyNode.java
@@ -0,0 +1,13 @@
+package data_04_Queue;
+
+public class SinglyNode<E> {
+	
+	E data;
+	SinglyNode<E> next; // 다음 연결할 노드를 가리키는 reference 변수
+	
+	// 생성자
+	SinglyNode(E data){
+		this.data = data;
+		this.next = null;
+	}
+}
